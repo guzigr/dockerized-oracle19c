7e2be5e076c457b8511c4fc670d56789c73d9b30
guzigr <technocraci@gmail.com>
2020-04-26 04:31:47 +0000

    change filename

---
+++ make_docker.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+docker-images/OracleDatabase/SingleInstance/dockerfiles$ ./buildDockerImage.sh -v 19.3.0 -e
---
+++ volume_install.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+sudo mkdir /home/oracle
+sudo chmod 777 /home/oracle
